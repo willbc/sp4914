4a40e66a4bb3dea3009667cc4f4761edcb1944d6
Will <wccase@live.com>
2016-10-20 18:22:49 -0400

    Created Ray Controller and tower node controller:

---
+++ Assets/Scripts/Ray_Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ray_Controller : MonoBehaviour {
+
+    public Ray mainRay;
+
+
+	void Start () {
+        mainRay.origin = transform.position;
+        mainRay.direction = transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mainRay.origin = transform.position;
+        mainRay.direction = transform.forward;
+        //Debug.Log(mainRay.origin);
+	}
+
+    public Ray GetMainRay() {
+        return mainRay;
+    }
+}
---
+++ Assets/Scripts/Tower_Node_Controller.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower_Node_Controller : MonoBehaviour {
+
+    public Ray playerRay;
+
+    public bool isHitByRay;
+
+    public float range = 1000.0f;
+
+    RaycastHit hit;
+
+
+	void Start () {
+        isHitByRay = false;
+        playerRay = GameObject.Find("Main Camera").GetComponent<Ray_Controller>().GetMainRay();
+        Debug.Log(playerRay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Physics.Raycast(playerRay, out hit));
+        if(Physics.Raycast (playerRay, out hit)) {
+            Debug.Log(hit);
+            if(hit.collider.tag == "TowerSpot") {
+                Debug.Log("hit");
+                isHitByRay = true;
+            }
+            else {
+                isHitByRay = false;
+            }
+        }
+        else if(isHitByRay) {
+            isHitByRay = false;
+        }
+
+        if(isHitByRay) {
+            transform.localScale = new Vector3(transform.localScale.x, 3F, transform.localScale.y);
+        }
+        else {
+            transform.localScale = new Vector3(transform.localScale.x, 1F, transform.localScale.y);
+        }
+	}
+}
