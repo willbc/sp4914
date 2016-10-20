d2b02be95ff686bab13f6f5ed0a0ea990a47aa14
golliher-sidney <golliher-sidney@gmail.com>
2016-10-20 18:15:06 -0400

    Made tower placement controller and tower spot scripts

---
+++ Assets/Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAi : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pointA.position, pointB.position, Mathf.Sin(Time.time * speed));
+    }
+}
---
+++ Assets/Scripts/Tower_Spot.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Tower_Spot : MonoBehaviour {
+
+    public Renderer rend;
+    bool towerPlaced;
+    bool overTowerSpot;
+    public GameObject towerspot;
+    public GameObject tower;
+    Color towerSpotStandbyColor = new Color(68, 230, 255, 147);
+    Color towerSpotHighlightColor = new Color(0, 255, 0, 147);
+
+    // Use this for initialization
+    void Start () {
+        towerPlaced = false;
+        overTowerSpot = false;
+        tower = null;
+        rend = GetComponent<Renderer>();
+        rend.material.color = towerSpotStandbyColor;
+    }
+
+    void HighlightColor(){
+        rend.material.color = towerSpotStandbyColor;
+    }
+
+    void StandbyColor(){
+        rend.material.color = towerSpotHighlightColor;
+    }
+
+    public void buildTower(KeyCode keyPressed){
+        if(keyPressed == KeyCode.E){
+            tower = Instantiate(Resources.Load("Assets/Prefabs/Tower1"), new Vector3(0,0,0), Quaternion.identity) as GameObject;
+        }
+    }
+    public void ObjectHitLessThanTwo(){
+        overTowerSpot = true;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        if (overTowerSpot){
+            HighlightColor();
+        }
+        else{
+            StandbyColor();
+        }
+	}
+}
