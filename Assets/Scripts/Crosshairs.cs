9893d9d9aa94796f74817954de1c993c93a4e06b
Will <wccase@live.com>
2016-12-05 19:31:23 -0500

    Fixed crosshairs to work with VR

--- Assets/Scripts/Crosshairs.cs
+++ Assets/Scripts/Crosshairs.cs
@@ -14,6 +14,6 @@ public class Crosshairs : MonoBehaviour {
 	}
 	
     void OnGUI() {
-        GUI.DrawTexture(crosshairRect, crosshairTexture);
+        //GUI.DrawTexture(crosshairRect, crosshairTexture);
     }
 }
